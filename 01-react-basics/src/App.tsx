2129d2e871790ece4bb59e36ff06b2ca24490b69
H1V35 <higesjorge@gmail.com>
2024-03-25 20:03:00 +0100

    ✨ feat: Add BasicTypes component to review basic TypeScript

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -1,7 +1,11 @@
+import { BasicTypes } from '@/typescript/BasicTypes';
+
 export default function App() {
   return (
     <main>
       <h1>TS + React Introduction</h1>
+
+      <BasicTypes />
     </main>
   );
 }
---
+++ 01-react-basics/src/typescript/BasicTypes.tsx
@@ -0,0 +1,19 @@
+export function BasicTypes() {
+  // Type inference
+  const name = 'John';
+  const age = 33;
+  const isActive = true;
+  const powers = ['TypeScript', 'React', 'ReactNative'];
+
+  powers.push('Next');
+
+  return (
+    <div className="mt-10">
+      <h3 className="text-2xl">Basic Types</h3>
+      <br />
+      {name} {age} {`Status: ${isActive ? 'Active' : 'Inactive'}`}
+      <br />
+      {`Powers: ${powers.join(', ')}`}
+    </div>
+  );
+}
