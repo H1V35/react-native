670cf2f1472adaabd775fbc8973bb719c6d79919
H1V35 <higesjorge@gmail.com>
2024-03-26 18:57:00 +0100

    ✨ feat: Add ObjectLiterals component to review object literals in TypeScript

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -1,11 +1,13 @@
-import { BasicTypes } from '@/typescript/BasicTypes';
+// import { BasicTypes } from '@/typescript/BasicTypes';
+import { ObjectLiterals } from '@/typescript/ObjectLiterals';
 
 export default function App() {
   return (
     <main>
       <h1>TS + React Introduction</h1>
 
-      <BasicTypes />
+      {/* <BasicTypes /> */}
+      <ObjectLiterals />
     </main>
   );
 }
---
+++ 01-react-basics/src/typescript/ObjectLiterals.tsx
@@ -0,0 +1,31 @@
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+  address: Address;
+}
+
+interface Address {
+  country: string;
+  houseNo: number;
+}
+
+export function ObjectLiterals() {
+  const person: Person = {
+    firstName: 'Fernando',
+    lastName: 'Alonso',
+    age: 42,
+    address: {
+      country: 'Spain',
+      houseNo: 33,
+    },
+  };
+
+  return (
+    <div className="mt-10">
+      <h3 className="text-2xl">Object Literals</h3>
+      <br />
+      <pre>{JSON.stringify(person, null, 2)}</pre>
+    </div>
+  );
+}
