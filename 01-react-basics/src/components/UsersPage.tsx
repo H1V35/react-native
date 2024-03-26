7c9fbf4cb047347bab022ed46a22a35e2aadd6a5
H1V35 <higesjorge@gmail.com>
2024-03-26 21:03:46 +0100

    ✨ feat: Implement basic pagination for results

--- 01-react-basics/src/components/UsersPage.tsx
+++ 01-react-basics/src/components/UsersPage.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import axios from 'axios';
 import type { ReqResUserListResponse, User } from '@/interfaces/reqres.interface';
 
-const loadUsers = async (): Promise<User[]> => {
+const loadUsers = async (page: number = 1): Promise<User[]> => {
   try {
-    const { data } = await axios.get<ReqResUserListResponse>('https://reqres.in/api/users');
+    const { data } = await axios.get<ReqResUserListResponse>('https://reqres.in/api/users', {
+      params: {
+        page: page,
+      },
+    });
     return data.data;
   } catch (error) {
     console.log(error);
@@ -14,14 +18,34 @@ const loadUsers = async (): Promise<User[]> => {
 
 export function UsersPage() {
   const [users, setUsers] = React.useState<User[]>([]);
+  const currentPageRef = React.useRef(1);
 
   React.useEffect(() => {
     // fetch('https://reqres.in/api/users?page=2')
     //   .then((res) => res.json())
     //   .then((data) => console.log(data));
 
-    loadUsers().then(setUsers);
+    loadUsers(currentPageRef.current).then(setUsers);
   }, []);
+
+  const nextPage = async () => {
+    currentPageRef.current++;
+    const users = await loadUsers(currentPageRef.current);
+
+    if (users.length > 0) {
+      setUsers(users);
+      return;
+    }
+    currentPageRef.current--;
+  };
+
+  const prevPage = async () => {
+    if (currentPageRef.current <= 1) return;
+
+    currentPageRef.current--;
+    const users = await loadUsers(currentPageRef.current);
+    setUsers(users);
+  };
 
   return (
     <div className="flex flex-col gap-8 text-center">
@@ -42,6 +66,11 @@ export function UsersPage() {
           ))}
         </tbody>
       </table>
+
+      <div className="flex justify-center gap-2">
+        <button onClick={prevPage}>Prev</button>
+        <button onClick={nextPage}>Next</button>
+      </div>
     </div>
   );
 }
