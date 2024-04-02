92fd157228562ecb9588eefe2cccb2b9d2d84267
H1V35 <higesjorge@gmail.com>
2024-04-02 19:27:20 +0200

    ✨ feat: Create first screen HelloWorldScreen

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -1,13 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { HelloWorldScreen } from '~/presentation/screens/HelloWorldScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <HelloWorldScreen />
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +14,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ 02-MyFirstApp/src/presentation/screens/HelloWorldScreen.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export function HelloWorldScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 45,
+    textAlign: 'center',
+    color: 'black',
+    padding: 20,
+  },
+});
