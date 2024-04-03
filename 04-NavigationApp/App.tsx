75225d780f08897d94eac2c9e4bf51420b19d05d
H1V35 <higesjorge@gmail.com>
2024-04-03 14:58:39 +0200

    📦 add: Add 'React Navigation' dependency

--- 04-NavigationApp/App.tsx
+++ 04-NavigationApp/App.tsx
@@ -1,16 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { Text } from 'react-native';
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <NavigationContainer>
       <Text>Hello World</Text>
-    </SafeAreaView>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
