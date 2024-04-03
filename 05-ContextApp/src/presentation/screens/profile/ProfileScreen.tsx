7c2710e747df28cb94f9f5aa4e1b0cc52d026bb0
H1V35 <higesjorge@gmail.com>
2024-04-03 19:19:09 +0200

    ✨ feat: Implement functionality to make changes in the store

--- 05-ContextApp/src/presentation/screens/profile/ProfileScreen.tsx
+++ 05-ContextApp/src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 
 import { globalStyles } from '~/config/theme/app-theme';
 import { useProfileStore } from '~/presentation/store/profile-store';
@@ -6,11 +6,30 @@ import { useProfileStore } from '~/presentation/store/profile-store';
 export function ProfileScreen() {
   const name = useProfileStore((state) => state.name);
   const email = useProfileStore((state) => state.email);
+  const changeProfile = useProfileStore((state) => state.changeProfile);
 
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.title}>{name}</Text>
       <Text style={globalStyles.title}>{email}</Text>
+
+      <Pressable
+        style={globalStyles.primaryButton}
+        onPress={() => useProfileStore.setState({ name: 'Hive' })}>
+        <Text>Change name</Text>
+      </Pressable>
+
+      <Pressable
+        style={globalStyles.primaryButton}
+        onPress={() => useProfileStore.setState({ email: 'hive@google.com' })}>
+        <Text>Change email</Text>
+      </Pressable>
+
+      <Pressable
+        style={globalStyles.primaryButton}
+        onPress={() => changeProfile('John Cena', 'john.cena@google.com')}>
+        <Text>IT'S JOHN CENAAA!</Text>
+      </Pressable>
     </View>
   );
 }
--- 05-ContextApp/src/presentation/store/profile-store.ts
+++ 05-ContextApp/src/presentation/store/profile-store.ts
@@ -3,9 +3,15 @@ import { create } from 'zustand';
 export interface ProfileState {
   name: string;
   email: string;
+
+  changeProfile: (name: string, email: string) => void;
 }
 
 export const useProfileStore = create<ProfileState>()((set, get) => ({
   name: 'John Cena',
   email: 'john.cena@google.com',
+
+  changeProfile: (name: string, email: string) => {
+    set({ name, email });
+  },
 }));
