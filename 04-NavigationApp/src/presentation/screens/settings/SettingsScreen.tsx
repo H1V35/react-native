3ff08d099f45d72ea9b1843b5116b2c261a4b1cc
H1V35 <higesjorge@gmail.com>
2024-04-03 15:58:06 +0200

    ✨ feat: Implement back and navigate to HomeScreen functionality in SettingsScreen

--- 04-NavigationApp/src/presentation/screens/settings/SettingsScreen.tsx
+++ 04-NavigationApp/src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,9 +1,22 @@
+import { StackActions, useNavigation } from '@react-navigation/native';
 import { Text, View } from 'react-native';
 
+import { globalStyles } from '~/config/theme/theme';
+import { PrimaryButton } from '~/presentation/components/shared/PrimaryButton';
+
 export function SettingsScreen() {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={globalStyles.container}>
+      <Text style={{ marginBottom: 10 }}>SettingsScreen</Text>
+
+      <PrimaryButton label="Back" onPress={() => navigation.goBack()} />
+
+      <PrimaryButton
+        label="Back to Home"
+        onPress={() => navigation.dispatch(StackActions.popToTop())}
+      />
     </View>
   );
 }
