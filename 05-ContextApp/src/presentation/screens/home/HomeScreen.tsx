bae23fd4b58052b825170338e05f9ebe57db07a7
H1V35 <higesjorge@gmail.com>
2024-04-03 19:10:58 +0200

    ✨ feat: Install 'Zustand' as state manager; Create profile-store

--- 05-ContextApp/src/presentation/screens/home/HomeScreen.tsx
+++ 05-ContextApp/src/presentation/screens/home/HomeScreen.tsx
@@ -1,11 +1,16 @@
 import { Text, View } from 'react-native';
 
 import { globalStyles } from '~/config/theme/app-theme';
+import { useProfileStore } from '~/presentation/store/profile-store';
 
 export function HomeScreen() {
+  const name = useProfileStore((state) => state.name);
+  const email = useProfileStore((state) => state.email);
+
   return (
     <View style={globalStyles.container}>
-      <Text>HomeScreen</Text>
+      <Text style={globalStyles.title}>{name}</Text>
+      <Text style={globalStyles.title}>{email}</Text>
     </View>
   );
 }
