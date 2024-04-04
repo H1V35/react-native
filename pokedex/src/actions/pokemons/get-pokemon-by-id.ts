220d75d739b6f1f5ec5e7aa65e4998aad4dc1ee1
H1V35 <higesjorge@gmail.com>
2024-04-04 16:57:01 +0200

    ✨ feat: Implement navigation to Pokemon details screen

---
+++ pokedex/src/actions/pokemons/get-pokemon-by-id.ts
@@ -0,0 +1,15 @@
+import { pokeApi } from '~/config/api/pokeApi';
+import { type Pokemon } from '~/domain/entities/pokemon';
+import { type PokeAPIPokemon } from '~/infraestructure/interfaces/pokeapi.interfaces';
+import { PokemonMapper } from '~/infraestructure/mappers/pokemon.mapper';
+
+export const getPokemonById = async (id: number): Promise<Pokemon> => {
+  try {
+    const { data } = await pokeApi.get<PokeAPIPokemon>(`/pokemon/${id}`);
+    const pokemon = await PokemonMapper.pokeApiPokemonToEntity(data);
+    return pokemon;
+  } catch (error) {
+    console.log(error);
+    throw new Error(`Error getting pokemon by id: ${id}`);
+  }
+};
--- pokedex/src/presentation/screens/pokemon/PokemonScreen.tsx
+++ pokedex/src/presentation/screens/pokemon/PokemonScreen.tsx
@@ -1,9 +1,24 @@
+import { StackScreenProps } from '@react-navigation/stack';
+import { useQuery } from '@tanstack/react-query';
 import { Text, View } from 'react-native';
 
-export function PokemonScreen() {
+import { getPokemonById } from '~/actions/pokemons/get-pokemon-by-id';
+import { RootStackParams } from '~/presentation/navigator/StackNavigator';
+
+interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'> {}
+
+export function PokemonScreen({ navigation, route }: Props) {
+  const { pokemonId } = route.params;
+
+  const { isLoading, data: pokemon } = useQuery({
+    queryKey: ['pokemon', pokemonId],
+    queryFn: () => getPokemonById(pokemonId),
+    staleTime: 1000 * 60 * 60,
+  });
+
   return (
     <View>
-      <Text>PokemonScreen</Text>
+      <Text>{pokemon?.name}</Text>
     </View>
   );
 }
