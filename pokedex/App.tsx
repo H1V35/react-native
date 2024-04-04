9283e9221f08f3b66e921731c9b2d9d06ee70ff9
H1V35 <higesjorge@gmail.com>
2024-04-04 13:44:12 +0200

    ✨ feat: Install and configure 'TanStack Query'

--- pokedex/App.tsx
+++ pokedex/App.tsx
@@ -1,12 +1,17 @@
 import 'react-native-gesture-handler';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { ThemeContextProvider } from '~/presentation/context/ThemeContext';
 import { StackNavigator } from '~/presentation/navigator/StackNavigator';
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <ThemeContextProvider>
-      <StackNavigator />
-    </ThemeContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeContextProvider>
+        <StackNavigator />
+      </ThemeContextProvider>
+    </QueryClientProvider>
   );
 }
--- pokedex/src/presentation/screens/home/HomeScreen.tsx
+++ pokedex/src/presentation/screens/home/HomeScreen.tsx
@@ -1,14 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
 import { View } from 'react-native';
-import { Button, Text } from 'react-native-paper';
+import { ActivityIndicator, Button, Text } from 'react-native-paper';
+
+import { getPokemons } from '~/actions/pokemons/get-pokemons';
 
 export function HomeScreen() {
+  const { isLoading, data } = useQuery({
+    queryKey: ['pokemons'],
+    queryFn: () => getPokemons(),
+    staleTime: 1000 * 60 * 60,
+  });
+
   return (
     <View>
       <Text variant="displaySmall">HomeScreen</Text>
 
-      <Button mode="contained" onPress={() => console.log('Pressed')}>
-        Press me
-      </Button>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <Button mode="contained" onPress={() => console.log('Pressed')}>
+          Press me
+        </Button>
+      )}
     </View>
   );
 }
