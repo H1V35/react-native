a0c027ca961c9516bb09dc26780d3222b40dfbef
H1V35 <higesjorge@gmail.com>
2024-04-04 11:27:48 +0200

    🔧 chore: Install 'react-native-dotenv' and configure environment variables

--- 06-MoviesApp/src/config/adapters/movieDB.adapter.ts
+++ 06-MoviesApp/src/config/adapters/movieDB.adapter.ts
@@ -1,9 +1,11 @@
+import { THE_MOVIE_DB_KEY } from '@env';
+
 import { AxiosAdapter } from './http/axios.adapter';
 
 export const movieDBFetcher = new AxiosAdapter({
   baseUrl: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: '9360b112085e5b54b56463a4f723c188',
+    api_key: THE_MOVIE_DB_KEY ?? 'no-key',
     language: 'en',
   },
 });
