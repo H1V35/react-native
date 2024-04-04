3621dbe0cb9ced74cd3b1ad7e861c374f16ac2a8
H1V35 <higesjorge@gmail.com>
2024-04-04 13:19:59 +0200

    ✨ feat: Implement light and dark theme automatically based on user preferences; Create ThemeContext

--- pokedex/App.tsx
+++ pokedex/App.tsx
@@ -1,15 +1,12 @@
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { PaperProvider } from 'react-native-paper';
 
+import { ThemeContextProvider } from '~/presentation/context/ThemeContext';
 import { StackNavigator } from '~/presentation/navigator/StackNavigator';
 
 export default function App() {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
-    </PaperProvider>
+    <ThemeContextProvider>
+      <StackNavigator />
+    </ThemeContextProvider>
   );
 }
