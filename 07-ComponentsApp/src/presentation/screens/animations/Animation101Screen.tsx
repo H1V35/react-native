d6a6c096fa1a4e1e6db6063b9dfea673b79462f4
H1V35 <higesjorge@gmail.com>
2024-04-10 16:10:07 +0200

    ✨ feat: Implement bounce easing animation on element in Animation101Screen

--- 07-ComponentsApp/src/presentation/screens/animations/Animation101Screen.tsx
+++ 07-ComponentsApp/src/presentation/screens/animations/Animation101Screen.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Animated, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Animated, Easing, Pressable, StyleSheet, Text, View } from 'react-native';
 
 import { colors } from '~/config/theme/theme';
 
 export function Animation101Screen() {
   const animatedOpacity = React.useRef(new Animated.Value(0)).current;
+  const animatedTop = React.useRef(new Animated.Value(-150)).current;
 
   const fadeIn = () => {
+    Animated.timing(animatedTop, {
+      toValue: 0,
+      duration: 600,
+      useNativeDriver: true,
+      easing: Easing.bounce,
+      // easing: Easing.elastic(1),
+    }).start(() => console.log('AnimationTop ended'));
+
     Animated.timing(animatedOpacity, {
       toValue: 1,
       duration: 300,
       useNativeDriver: true,
-    }).start(() => console.log('Animation ended'));
+    }).start(() => console.log('AnimationFadeIn ended'));
   };
 
   const fadeOut = () => {
@@ -19,12 +28,24 @@ export function Animation101Screen() {
       toValue: 0,
       duration: 300,
       useNativeDriver: true,
-    }).start(() => console.log('Animation ended'));
+    }).start(() => animatedTop.resetAnimation());
   };
 
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.purpleBox, { opacity: animatedOpacity }]} />
+      <Animated.View
+        style={[
+          styles.purpleBox,
+          {
+            opacity: animatedOpacity,
+            transform: [
+              {
+                translateY: animatedTop,
+              },
+            ],
+          },
+        ]}
+      />
 
       <Pressable onPress={fadeIn} style={{ marginTop: 10 }}>
         <Text>FadeIn</Text>
