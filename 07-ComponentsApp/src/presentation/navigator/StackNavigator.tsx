0bc0a13985801b0c749532ddf9a4e324e4e7bd90
H1V35 <higesjorge@gmail.com>
2024-04-10 17:24:16 +0200

    ✨ feat: Create Animation102Screen to implement Animated ValueXY animation; Add screen to navigation

--- 07-ComponentsApp/src/presentation/navigator/StackNavigator.tsx
+++ 07-ComponentsApp/src/presentation/navigator/StackNavigator.tsx
@@ -1,6 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { Animation101Screen } from '~/presentation/screens/animations/Animation101Screen';
+import { Animation102Screen } from '~/presentation/screens/animations/Animation102Screen';
 import { HomeScreen } from '~/presentation/screens/home/HomeScreen';
 
 const Stack = createStackNavigator();
@@ -13,6 +14,7 @@ export function StackNavigator() {
       }}>
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="Animation101Screen" component={Animation101Screen} />
+      <Stack.Screen name="Animation102Screen" component={Animation102Screen} />
     </Stack.Navigator>
   );
 }
