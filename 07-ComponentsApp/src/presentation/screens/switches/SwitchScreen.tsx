81b966c2532813f5b7ed0cbb8dc94feff1ddb79e
H1V35 <higesjorge@gmail.com>
2024-04-11 22:52:01 +0200

    ✨ feat: Create CustomSwitch UI component and implement in SwitchScreen

--- 07-ComponentsApp/src/presentation/screens/switches/SwitchScreen.tsx
+++ 07-ComponentsApp/src/presentation/screens/switches/SwitchScreen.tsx
@@ -1,12 +1,36 @@
-import { Button } from '~/presentation/components/ui/Button';
+import React from 'react';
+
 import { Card } from '~/presentation/components/ui/Card';
+import { CustomSwitch } from '~/presentation/components/ui/CustomSwitch';
 import { CustomView } from '~/presentation/components/ui/CustomView';
 
 export function SwitchScreen() {
+  const [state, setState] = React.useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true,
+  });
+
   return (
     <CustomView style={{ marginTop: 100, paddingHorizontal: 10 }}>
       <Card>
-        <Button text="Click me" onPress={() => {}} />
+        <CustomSwitch
+          isOn={state.isActive}
+          onChange={(value) => setState({ ...state, isActive: value })}
+          text="Are you active?"
+        />
+
+        <CustomSwitch
+          isOn={state.isHungry}
+          onChange={(value) => setState({ ...state, isHungry: value })}
+          text="Are you hungry?"
+        />
+
+        <CustomSwitch
+          isOn={state.isHappy}
+          onChange={(value) => setState({ ...state, isHappy: value })}
+          text="Are you happy?"
+        />
       </Card>
     </CustomView>
   );
