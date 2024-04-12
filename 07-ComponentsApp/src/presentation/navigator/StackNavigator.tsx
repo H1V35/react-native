6c8187c5d1d1e67374f32af2d4a5194635451b56
H1V35 <higesjorge@gmail.com>
2024-04-12 17:05:16 +0200

    ✨ feat: Create AlertScreen and implement in navigator

--- 07-ComponentsApp/src/presentation/navigator/StackNavigator.tsx
+++ 07-ComponentsApp/src/presentation/navigator/StackNavigator.tsx
@@ -1,5 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack';
 
+import { AlertScreen } from '~/presentation/screens/alerts/AlertScreen';
 import { Animation101Screen } from '~/presentation/screens/animations/Animation101Screen';
 import { Animation102Screen } from '~/presentation/screens/animations/Animation102Screen';
 import { HomeScreen } from '~/presentation/screens/home/HomeScreen';
@@ -16,7 +17,9 @@ export function StackNavigator() {
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="Animation101Screen" component={Animation101Screen} />
       <Stack.Screen name="Animation102Screen" component={Animation102Screen} />
+
       <Stack.Screen name="SwitchScreen" component={SwitchScreen} />
+      <Stack.Screen name="AlertScreen" component={AlertScreen} />
     </Stack.Navigator>
   );
 }
---
+++ 07-ComponentsApp/src/presentation/screens/alerts/AlertScreen.tsx
@@ -0,0 +1,55 @@
+import { Alert, View } from 'react-native';
+
+import { Button } from '~/presentation/components/ui/Button';
+import { CustomView } from '~/presentation/components/ui/CustomView';
+import { Title } from '~/presentation/components/ui/Title';
+
+export function AlertScreen() {
+  const createTwoButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'destructive',
+      },
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ]);
+
+  const createThreeButtonAlert = () =>
+    Alert.alert(
+      'Alert Title',
+      'My Alert Msg',
+      [
+        { text: 'OK', onPress: () => console.log('OK Pressed') },
+        {
+          text: 'Ask me later',
+          onPress: () => console.log('Ask me later pressed'),
+        },
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'destructive',
+        },
+      ],
+      {
+        cancelable: true,
+        onDismiss() {
+          console.log('onDismiss');
+        },
+      }
+    );
+
+  return (
+    <CustomView>
+      <Title safe text="Alerts" />
+
+      <Button text="Alert - 2 Buttons" onPress={createTwoButtonAlert} />
+      <View style={{ height: 10 }} />
+
+      <Button text="Alert - 3 Buttons" onPress={createThreeButtonAlert} />
+      <View style={{ height: 10 }} />
+
+      <Button text="Prompt - Input" onPress={() => {}} />
+    </CustomView>
+  );
+}
