c9d0f01a26933373831a21c8fc7623e30bb62f27
H1V35 <higesjorge@gmail.com>
2024-04-13 21:18:14 +0200

    ✨ feat: Implement images and loader in InfiniteScrollScreen

--- 07-ComponentsApp/App.tsx
+++ 07-ComponentsApp/App.tsx
@@ -1,13 +1,11 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
 
 import { StackNavigator } from '~/presentation/navigator/StackNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar style="auto" />
       <StackNavigator />
     </NavigationContainer>
   );
--- 07-ComponentsApp/src/presentation/screens/ui/InfiniteScrollScreen.tsx
+++ 07-ComponentsApp/src/presentation/screens/ui/InfiniteScrollScreen.tsx
@@ -1,40 +1,47 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { ActivityIndicator, Image, View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 
 import { colors } from '~/config/theme/theme';
-import { CustomView } from '~/presentation/components/ui/CustomView';
-import { Title } from '~/presentation/components/ui/Title';
 
 export function InfiniteScrollScreen() {
   const [numbers, setNumbers] = React.useState([0, 1, 2, 3, 4, 5]);
 
   const loadMore = () => {
     const newArray = Array.from({ length: 5 }, (_, i) => numbers.length + i);
-    setNumbers([...numbers, ...newArray]);
+
+    setTimeout(() => {
+      setNumbers([...numbers, ...newArray]);
+    }, 3000);
   };
 
   return (
-    <CustomView margin>
-      <Title text="Infinite Scroll" safe />
-
+    <View style={{ backgroundColor: 'black' }}>
       <FlatList
         data={numbers}
         onEndReached={loadMore}
         onEndReachedThreshold={0.6}
         keyExtractor={(item) => item.toString()}
-        renderItem={({ item }) => (
-          <Text
-            style={{
-              height: 300,
-              backgroundColor: colors.primary,
-              color: 'white',
-              fontSize: 60,
-            }}>
-            {item}
-          </Text>
+        renderItem={({ item }) => <ListItem number={item} />}
+        ListFooterComponent={() => (
+          <View style={{ height: 150, justifyContent: 'center' }}>
+            <ActivityIndicator size={30} color={colors.primary} />
+          </View>
         )}
       />
-    </CustomView>
+    </View>
+  );
+}
+
+interface ListItemProps {
+  number: number;
+}
+
+function ListItem({ number }: ListItemProps) {
+  return (
+    <Image
+      source={{ uri: `https://picsum.photos/id/${number}/500/400` }}
+      style={{ height: 400, width: '100%' }}
+    />
   );
 }
