4e9f517eddbd6a00363d77d8cfed0ebf5a4ee9d3
H1V35 <higesjorge@gmail.com>
2024-04-15 12:42:37 +0200

    ✨ feat: Create SlideItem and implement in SlidesScreen; Add graphic resources in assets

--- 07-ComponentsApp/src/presentation/screens/ui/SlidesScreen.tsx
+++ 07-ComponentsApp/src/presentation/screens/ui/SlidesScreen.tsx
@@ -1,9 +1,94 @@
-import { Text, View } from 'react-native';
+import { Image, ImageSourcePropType, Text, View, useWindowDimensions } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+
+import { colors, globalStyles } from '~/config/theme/theme';
+
+interface Slide {
+  title: string;
+  desc: string;
+  img: ImageSourcePropType;
+}
+
+const items: Slide[] = [
+  {
+    title: 'Titulo 1',
+    desc: 'Ea et eu enim fugiat sunt reprehenderit sunt aute quis tempor ipsum cupidatat et.',
+    img: require('~/presentation/assets/slide-1.png'),
+  },
+  {
+    title: 'Titulo 2',
+    desc: 'Anim est quis elit proident magna quis cupidatat curlpa labore Lorem ea. Exercitation mollit velit in aliquip tempor occaecat dolor minim amet dolor enim cillum excepteur. ',
+    img: require('~/presentation/assets/slide-2.png'),
+  },
+  {
+    title: 'Titulo 3',
+    desc: 'Ex amet duis amet nulla. Aliquip ea Lorem ea culpa consequat proident. Nulla tempor esse ad tempor sit amet Lorem. Velit ea labore aute pariatur commodo duis veniam enim.',
+    img: require('~/presentation/assets/slide-3.png'),
+  },
+];
 
 export function SlidesScreen() {
   return (
-    <View>
-      <Text>SlidesScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: colors.background,
+      }}>
+      <FlatList
+        data={items}
+        keyExtractor={(item) => item.title}
+        renderItem={({ item }) => <SlideItem item={item} />}
+        horizontal
+        pagingEnabled
+      />
     </View>
   );
 }
+
+interface SlideItemProps {
+  item: Slide;
+}
+
+const SlideItem = ({ item: { title, desc, img } }: SlideItemProps) => {
+  const { width } = useWindowDimensions();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        borderRadius: 5,
+        padding: 40,
+        justifyContent: 'center',
+        width,
+      }}>
+      <Image
+        source={img}
+        style={{
+          width: width * 0.7,
+          height: width * 0.7,
+          resizeMode: 'center',
+          alignSelf: 'center',
+        }}
+      />
+
+      <Text
+        style={[
+          globalStyles.title,
+          {
+            color: colors.primary,
+          },
+        ]}>
+        {title}
+      </Text>
+
+      <Text
+        style={{
+          color: colors.text,
+          marginTop: 20,
+        }}>
+        {desc}
+      </Text>
+    </View>
+  );
+};
