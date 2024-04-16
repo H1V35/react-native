d9217ac79d3efd34a0fc7868cae851b3cef2c154
H1V35 <higesjorge@gmail.com>
2024-04-16 19:50:13 +0200

    ✨ feat: Add StatusBar; Configure app to automatically change theme based on device theme

--- pokedex/App.tsx
+++ pokedex/App.tsx
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { StatusBar } from 'expo-status-bar';
 
 import { ThemeContextProvider } from '~/presentation/context/ThemeContext';
 import { StackNavigator } from '~/presentation/navigator/StackNavigator';
@@ -10,6 +11,7 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeContextProvider>
+        <StatusBar style="auto" />
         <StackNavigator />
       </ThemeContextProvider>
     </QueryClientProvider>
